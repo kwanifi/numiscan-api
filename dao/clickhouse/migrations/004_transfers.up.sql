a9f65ca8bbe05abcad8d66902b0f3fbebd96e924
John Reitano <jreitano@gmail.com>
2021-06-05 23:15:20 -0700

    fix sql error

--- dao/clickhouse/migrations/004_transfers.up.sql
+++ dao/clickhouse/migrations/004_transfers.up.sql
@@ -6,7 +6,9 @@ create table transfers
     trf_to         FixedString(45),
     trf_amount     Decimal128(18),
     trf_created_at DateTime,
-    trf_currency   String,
+    trf_currency   String
 ) ENGINE ReplacingMergeTree()
       PARTITION BY toYYYYMMDD(trf_created_at)
       ORDER BY (trf_id);
+
+drop table transfers;
